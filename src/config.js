3fcbad3a95ea1930b68fc6ebcbea8103062096e8
Ivan Beltran <56616238+Serebroinna@users.noreply.github.com>
2023-06-22 19:05:01 -0600

    Actualizacion a config.js para railway

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000
-export const DB_NAMEDATABASE = process.env.DB_DATABASE || 'sistema_elecciones'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'murc13l@g0'
+export const DB_NAMEDATABASE = process.env.DB_DATABASE || 'railway'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '2JLrkLWaxMzVzKdhPQlg'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-115.railway.app'
+export const DB_PORT = process.env.DB_PORT || 7796
