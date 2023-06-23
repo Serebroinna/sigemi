34a9ecc111b67ee6556058799eaa9f68115b7e75
Ivan Beltran <ivanbeltran1704@gmail.com>
2023-06-22 19:40:34 -0600

    mas correcciones a config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000
-export const DB_NAMEDATABASE = process.env.DB_DATABASE || ''
+export const DB_DATABASE = process.env.DB_DATABASE || ''
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DB_USER = process.env.DB_USER || ''
 export const DB_HOST = process.env.DB_HOST || ''
