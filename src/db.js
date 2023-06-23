853c670e18b24898df8c9f1a597e720344245f7f
Ivan Beltran <ivanbeltran1704@gmail.com>
2023-06-22 19:47:37 -0600

    ajustes en config.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import {createPool} from 'mysql2/promise'
-import { DB_HOST,DB_DATABASE,DB_PASSWORD,DB_PORT,DB_USER } from './config.js';
+import { DB_HOST,DB_NAMEDATABASE,DB_PASSWORD,DB_PORT,DB_USER } from './config.js';
 
 export const pool = createPool({
     user: DB_USER,
     password: DB_PASSWORD,
     host: DB_HOST,
     port: DB_PORT,
-    database: DB_DATABASE
+    database: DB_NAMEDATABASE
 });
